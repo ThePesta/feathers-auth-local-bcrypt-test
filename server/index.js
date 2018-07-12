0716afb3fb0860388a6faaf0dc54b95b9cb4dfa8
Jason Cheung <jckaichu@gmail.com>
2018-07-12 10:36:25 +0100

    added feathers auth, users service

--- server/index.js
+++ server/index.js
@@ -1,32 +1,74 @@
-const feathers = require('@feathersjs/feathers');
-const express = require('@feathersjs/express');
-const socketio = require('@feathersjs/socketio');
+const feathers = require('@feathersjs/feathers')
+const express = require('@feathersjs/express')
+const socketio = require('@feathersjs/socketio')
+const authentication = require('@feathersjs/authentication')
+const local = require('@feathersjs/authentication-local')
+const jwt = require('@feathersjs/authentication-jwt')
+const { times } = require('lodash')
 
-const memory = require('feathers-memory');
+const memory = require('feathers-memory')
 
 // Creates an Express compatible Feathers application
-const app = express(feathers());
+const app = express(feathers())
 
 // Parse HTTP JSON bodies
-app.use(express.json());
+app.use(express.json())
 // Parse URL-encoded params
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 // Add REST API support
-app.configure(express.rest());
+app.configure(express.rest())
 // Configure Socket.io real-time APIs
-app.configure(socketio());
+app.configure(socketio())
+
+app.configure(authentication({
+  secret: '123'
+}))
+
+app.configure(local())
+app.configure(jwt())
+
+
 // Register a messages service with pagination
 app.use('/messages', memory({
   paginate: {
     default: 10,
     max: 25
   }
-}));
+}))
+
+
+app.use('/users', memory())
+
+const usersService = app.service('users')
+
+usersService.hooks({
+  before: {
+    create: [
+      local.hooks.hashPassword()
+    ]
+  }
+})
+
+usersService.create(times(5, num => (
+  {
+    email: `${num + 1}@example.com`,
+    password: '12345'
+  }
+)))
+
+app.service('authentication').hooks({
+  before: {
+    create: [
+      authentication.hooks.authenticate(['local', 'jwt'])
+    ]
+  }
+})
+
 // Register a nicer error handler than the default Express one
-app.use(express.errorHandler());
+app.use(express.errorHandler())
 
 // Add any new real-time connection to the `everybody` channel
-app.on('connection', connection => app.channel('everybody').join(connection));
+app.on('connection', connection => app.channel('everybody').join(connection))
 // Publish all events to the `everybody` channel
 
 app.listen(3031).on('listening', () => {
